dcbf47e3131b766dd45f6e023ff0123af1f335c1
Smichmich <michael.alimi2@gmail.com>
2024-06-10 15:54:56 +0300

    Backend: Added get recepie by id controller

--- patisserie-back/src/api/controllers/recepie.controller.js
+++ patisserie-back/src/api/controllers/recepie.controller.js
@@ -7,7 +7,6 @@ const logger = require('../../config/logger');
 */
 exports.CreateRecepie = async (req, res, next) => {
   logger.info('Attempting to createa a new recepie');
-  logger.debug(req.body);
   await Recepie.create(req.body).then((newRecepie) => {
     logger.info(`Created a new recepie ${newRecepie}`);
     return res.status(200).json({ newRecepie });
@@ -25,4 +24,21 @@ exports.GetAllRecepies = async (req, res, next) => {
   logger.info('requested for all recepies');
   const recepies = await Recepie.find({});
   res.status(200).json({ recepies });
+};
+
+/*
+* Query a single recepie using it's id
+* @public
+*/
+exports.GetRecepieByID = async (req, res, next) => {
+  logger.info('Requesting recepie by id');
+  const { ID } = req.body;
+  logger.info(`Requested recepie id is ${ID}`);
+
+  const recepie = await Recepie.find({ _id: ID });
+  if (recepie) {
+    res.status(200).json(recepie);
+  } else {
+    res.status(404).json({ msg: 'Recepie not found' });
+  }
 };
