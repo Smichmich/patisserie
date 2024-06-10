e08c7056534c3b4ee99c0dce433b8fbc2b582213
Smichmich <michael.alimi2@gmail.com>
2024-06-10 15:35:51 +0300

    Front: Created view-recepie component

---
+++ patisserie-front/src/app/view-recepie/view-recepie.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-recepie',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-recepie.component.html',
+  styleUrl: './view-recepie.component.css'
+})
+export class ViewRecepieComponent {
+
+}
