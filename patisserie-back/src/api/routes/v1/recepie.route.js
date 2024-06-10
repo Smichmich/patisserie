bb56e0a0ed19e5bd0bc19b450ee7fa463c6c4baa
Smichmich <michael.alimi2@gmail.com>
2024-06-10 16:08:21 +0300

    Backend: changed route of single recepie to /v1/recepies/id

--- patisserie-back/src/api/routes/v1/recepie.route.js
+++ patisserie-back/src/api/routes/v1/recepie.route.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.route('/add').post(controller.CreateRecepie);
 router.route('/').get(controller.GetAllRecepies);
-router.route('/recepie/:ID').get(controller.GetRecepieByID);
+router.route('/:ID').get(controller.GetRecepieByID);
 
 module.exports = router;
