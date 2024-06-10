9362c2a4bfa4eb8d5074ced6c9184214fb8aa716
Smichmich <michael.alimi2@gmail.com>
2024-06-10 17:25:23 +0300

    Front: Built view-recepie component

--- patisserie-front/src/app/view-recepie/view-recepie.component.ts
+++ patisserie-front/src/app/view-recepie/view-recepie.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
-
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Recepie } from '../../models/recepie.model';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-view-recepie',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule, FormsModule],
   templateUrl: './view-recepie.component.html',
   styleUrl: './view-recepie.component.css'
 })
 export class ViewRecepieComponent {
 
+  backendUrl = 'http://localhost:3000/v1/recepies/'
+  recepie: Recepie | null = null;
+  recepieBackup: Recepie | null = null;
+  factor: number = 1;
+  constructor(private activeRoute: ActivatedRoute, private http: HttpClient) {}
+
+  ngOnInit() {
+    const recepieID = this.activeRoute.snapshot.params['recepieID'];
+    this.http.get<any>(`${this.backendUrl}/${recepieID}`).subscribe(res => {
+       this.recepie = res[0];
+       this.recepieBackup = JSON.parse(JSON.stringify(this.recepie))
+    })
+  }
+
+  multipleIngredients() {
+    this.recepie?.ingredients.forEach(ingredient => {
+      ingredient.amount = ingredient.amount * this.factor
+    });
+  }
+
+  resetRecepie() {
+    this.recepie = JSON.parse(JSON.stringify(this.recepieBackup))
+  }
 }
