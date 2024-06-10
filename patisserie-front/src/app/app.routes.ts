befab3c459bfd58a7adf5b49dc43fe047779c26f
Smichmich <michael.alimi2@gmail.com>
2024-06-10 15:38:41 +0300

    Front: Added view recepie component router

--- patisserie-front/src/app/app.routes.ts
+++ patisserie-front/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { CreateRecepieComponent } from './create-recepie/create-recepie.component';
 import { RecepiesListComponent } from './recepies-list/recepies-list.component';
 import { HomeComponent } from './home/home.component';
+import { ViewRecepieComponent } from './view-recepie/view-recepie.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'createRecepie', component: CreateRecepieComponent },
-    { path: 'recepies', component: RecepiesListComponent }
+    { path: 'recepies', component: RecepiesListComponent },
+    { path: 'recepie/:recepieID', component: ViewRecepieComponent }
 ];
