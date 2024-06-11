4afcfe3616dc997bc2b45762902c89b31e269b37
Smichmich <michael.alimi2@gmail.com>
2024-06-11 22:25:36 +0300

    Front: added functionality to edit recepie component

--- patisserie-front/src/app/edit-recepie/edit-recepie.component.ts
+++ patisserie-front/src/app/edit-recepie/edit-recepie.component.ts
@@ -3,11 +3,12 @@ import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { Recepie } from '../../models/recepie.model';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-recepie',
   standalone: true,
-  imports: [ RouterModule, CommonModule ],
+  imports: [ RouterModule, CommonModule, FormsModule ],
   templateUrl: './edit-recepie.component.html',
   styleUrl: './edit-recepie.component.css'
 })
@@ -15,11 +16,49 @@ import { CommonModule } from '@angular/common';
 export class EditRecepieComponent {
   constructor(private http: HttpClient, private activatedRoute: ActivatedRoute) {  }
   recepieToEdit: Recepie | null = null;
+  ingredientAmountToAdd: number = 0;
+  ingredientNameToAdd: string = '';
+  instructionToAdd: string = '';
 
   ngOnInit() {
     const id = this.activatedRoute.snapshot.params['recepieID'];
     this.http.get<Recepie[]>(`/v1/recepies/${id}`).subscribe((res) => {
       this.recepieToEdit = res[0];
+    })
+  }
+
+  addIngredient() {
+    this.recepieToEdit?.ingredients.push(
+      {name: this.ingredientNameToAdd, amount: this.ingredientAmountToAdd}
+    );
+    this.ingredientAmountToAdd = 0;
+    this.ingredientNameToAdd = '';
+  }
+
+  removeIngredient(index: number) {
+    this.recepieToEdit?.ingredients.splice(index, 1);
+  }
+
+  addInstruction() {
+    this.recepieToEdit?.instructions.push(
+      this.instructionToAdd
+    );
+    this.instructionToAdd = '';
+  }
+
+  removeInstruction(index: number) {
+    this.recepieToEdit?.instructions.splice(index, 1);
+  }
+
+  updateRecepie() {
+    this.http.put<any>(`/v1/recepies/${this.recepieToEdit?._id}`, this.recepieToEdit).subscribe((res) => {
+      console.log(res);
+    })
+  }
+
+  deleteRecepie() {
+    this.http.delete<any>(`/v1/recepies/${this.recepieToEdit?._id}`).subscribe((res) => {
+      console.log(res);
     })
   }
 }
